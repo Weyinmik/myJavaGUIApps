a979520724020d72c7414109046eba287d48b101
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-03 19:24:06 +0100

    added HelloComponent Class that etends JComponet.

--- src/chap1/JavaHello.java
+++ src/chap1/JavaHello.java
@@ -1,17 +1,25 @@
 package chap1;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaHello {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hello, Java");
-		JLabel label = new JLabel("Hello, Java", JLabel.CENTER);
-		frame.getContentPane().add(label);
+		//JLabel label = new JLabel("Hello, Java", JLabel.CENTER);
+		frame.getContentPane().add(new HelloComponent());
 		
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 
 	}
 
+}
+
+// Add a new HelloComponent class
+class HelloComponent extends JComponent{
+	public void paintComponent(Graphics g) {
+		g.drawString("Hello, Java", 125, 95);
+	}
 }
